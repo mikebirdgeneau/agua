61e4014f87043242da0a63fa06c3af55ea7009ba
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2024-02-20 10:50:02 -0700

    Update server.R
    Should only warn java not found, when has_java returns false.

--- R/server.R
+++ R/server.R
@@ -11,7 +11,7 @@
 #' @rdname h2o-server
 #' @export
 h2o_start <- function() {
-  if (has_java()) {
+  if (!has_java()) {
     rlang::warn("JAVA not found, H2O may take minutes trying to connect.")
   }
   res <- utils::capture.output(h2o::h2o.no_progress(
